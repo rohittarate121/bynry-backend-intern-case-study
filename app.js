5104b62a722be9d6f42e79fa20d718f53265f3f6
rohit <rohit.tarate121@gmail.com>
2025-12-30 00:09:26 +0530

    Add backend intern case study solution

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { sequelize } = require('./models');
+const { sequelize } = require("./models");
 
 app.use(express.json());
 
 // Routes
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/companies', require('./routes/alertRoutes'));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/companies", require("./routes/alertRoutes"));
 
 const PORT = 3000;
 
 sequelize.sync({ force: true }).then(() => {
-  console.log('Database synced');
+  console.log("Database synced");
   app.listen(PORT, () => {
     console.log(`http://localhost:3000/ ${PORT}`);
   });
